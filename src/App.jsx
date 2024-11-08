3a6c3ae8e9a4392b6adfd1f96056e716f86e9b35
Iago Torres <iagoportal10@gmail.com>
2024-11-08 15:27:27 -0300

    calucura

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App(){
+  return (
+    <main>
+      <h1>Calculadora</h1>
+      <input type="number" placeholder="Insira um número" />
+      <input type="number" placeholder="Insira um número" />
+
+      <div>
+        <button>+</button>
+        <button>-</button>
+        <button>/</button>
+        <button>x</button>
+      </div>
+    </main>
+  )
+}
