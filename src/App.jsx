320f7d04fa8eecdb5c4bc36b4c7d42aef5be534d
Iago Torres <iagoportal10@gmail.com>
2024-11-13 16:13:38 -0300

    atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react"
+//hook do react para mudanças de estado
 import "./style.scss"
+
 export default function App(){
   //VARIAVEL DE ESTADO
-  const [primeiroValor, setSegundoValor] =useState()
+  //primeiroValor é a varial de estado
+  //setPrimeiroValor é a função atualizadora
+  const [primeiroValor, setPrimeiroValor] =useState();
+  const [segundoValor, setSegundoValor] =useState();
+
+  //arrow function () => {}
+    //função especial
+  const capturandoPrimeiroValor = (e) => {
+    setSegundoValor(Number(e.target.value))
+  }
+
+  const capturandoSegundoValor  = (e) => {
+    setSegundoValor(Number(e.targaet.value))
+  }
   return (
     <main>
       <h1>Calculadora</h1>
-      <input type="number" placeholder="Insira um número" />
-      <input type="number" placeholder="Insira um número" />
+      <input onChange={capturandoPrimeiroValor} type="number" placeholder="Insira um número" />
+      <input onChange={capturandoSegundoValor} type="number" placeholder="Insira um número" />
 
       <div>
         <button>+</button>
